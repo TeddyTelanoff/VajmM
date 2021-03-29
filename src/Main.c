bcfb747c767180bb278b765b674a7ae143ffc953
Treidex <treidexsemextetry@gmail.com>
2021-03-29 16:02:45 -0700

    simple instruction reader

--- src/Main.c
+++ src/Main.c
@@ -28,6 +28,8 @@ const char *const RegKindNames[RegCount] =
 
 typedef enum
 {
+	IntRet,
+
 	IntMov,
 	IntMovVal,
 
@@ -39,12 +41,12 @@ typedef enum
 	IntCount,
 } IntKind;
 
-int Reg[RegCount] = { 0 };
+char Reg[RegCount] = { 0 };
 
 void MovInt(RegKind from, RegKind to)
 { Reg[to] = Reg[from]; }
 
-void MovValInt(int val, RegKind to)
+void MovValInt(char val, RegKind to)
 { Reg[to] = val; }
 
 void AddInt(RegKind from, RegKind to)
@@ -53,17 +55,48 @@ void AddInt(RegKind from, RegKind to)
 void PrintRegs()
 {
 	for (RegKind reg = 0; reg < RegCount; reg++)
-		printf("%s: %i\n", RegKindNames[reg], Reg[reg]);
+		printf("%s: %hhi\n", RegKindNames[reg], Reg[reg]);
 	putchar('\n');
 }
+
+void ExecuteProgram(const char *prog)
+{
+	IntKind $int;
+	char args[4];
+
+	while (*prog != IntRet)
+	{
+		switch (*prog++)
+		{
+		case IntMovVal:
+			args[0] = *prog++;
+			args[1] = *prog++;
+			MovValInt(args[0], args[1]);
+			break;
+
+		case IntAdd:
+			args[0] = *prog++;
+			args[1] = *prog++;
+			AddInt(args[0], args[1]);
+			break;
+		}
+	}
+}
+
+char myProg[] =
+{
+	IntMovVal, 60, RegAx,
+	IntMovVal, 9, RegBx,
+	IntAdd, RegBx, RegAx,
+
+	IntRet,
+};
 
 int main()
 {
 	PrintRegs();
 
-	MovValInt(60, RegAx);
-	MovValInt( 9, RegBx);
-	AddInt(RegBx, RegAx);
+	ExecuteProgram(myProg);
 
 	PrintRegs();
 
